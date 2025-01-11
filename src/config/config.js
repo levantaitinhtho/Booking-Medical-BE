5ab90edef757afeb41b39adf30e91a5197a25310
Lê Văn Tài ( Shy ) <88328633+levantaitinhtho@users.noreply.github.com>
2025-01-11 18:13:34 +0700

    moi1 vantai

--- src/config/config.js
+++ src/config/config.js
@@ -1,38 +1,39 @@
-require('dotenv').config(); // this is important!
+import config from "../config/config.js";
+require("dotenv").config(); // Đảm bảo rằng file .env được load
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
-    "define": {
-      "freezeTableName": true
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres", // Sử dụng giá trị từ .env, nếu không có sẽ mặc định là postgres
+    define: {
+      freezeTableName: true,
     },
     dialectOptions:
-      process.env.DB_SSL === 'true' ?
-        {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
+      process.env.DB_SSL === "true"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
           }
-        } : {}
-    ,
-    "timezone": "+07:00"
+        : {},
+    timezone: "+07:00",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
 };
